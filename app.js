110ae7588087277951f8765bfd1576c756341b87
Eskil Fogelström <eskilfogelstrom@gmail.com>
2023-10-03 08:51:05 +0200

    Add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const logger = require('morgan');
 
 const db = require('./db');
@@ -11,6 +12,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
